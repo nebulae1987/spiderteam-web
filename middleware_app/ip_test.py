ec2022f115dd764c2de8d7a04e7c1701db96ccc6
maxt <maxtang.5551@163.com>
2018-10-26 10:00:36 +0800

    提交了中间件（用户日志）的ip_test.py

---
+++ middleware_app/ip_test.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/10/25 9:46
+# @Author  : mxt
+# @Site    :
+# @File    : ip_test.py
+# @Software: PyCharm
+import datetime
+import urllib
+from urllib import request
+import re
+import happybase
+# try:
+#     ipaddr = '118.26.72.101'
+#     # ipaddr = raw_input("Enter IP Or Domain Name:")
+#     if ipaddr != "" or ipaddr != 'exit':
+#         url = "http://www.ip138.com/ips138.asp?ip=" + ipaddr + "&action=2"
+#         print(url)
+#         u = urllib.request.urlopen(url)
+#         s = u.read()
+#         s = s.decode('gbk')
+#         # Get IP Address
+#         ip = re.findall(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', s)
+#         request_time = datetime.datetime.now().strftime('%Y-%m-%d')
+#         print(request_time)
+#         print("\n****** Below Result From IP138 Database *****")
+#         print("IP Address:", ip[0])
+#         # Get IP Address Location
+#         result = re.findall(r'(<li>.*?</li>)', s)
+#         for i in result:
+#             info=''.join(i[4:-5])
+#             print(info)
+#         # print("*" * 45)
+#         # print("\n")
+# except Exception:
+#     print("Not Data Find")
+
+conn = happybase.Connection(host='192.168.0.99', port=9090, autoconnect=False)
+conn.open()
+table = conn.table('spiderteam:t_logs')
+# query="RowFilter(=,'substring:127.0.0.1')"
+t=table.scan()
+for i in t:
+    print(bytes.decode(i[1]['info:request_time']))
+
+
+
+
