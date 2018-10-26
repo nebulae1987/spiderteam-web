5e9b6fa27e55ef63dbce0911151a26e6093c8554
zj <704972565@qq.com>
2018-10-26 08:57:43 +0800

    添加招聘表model

--- sys_main/models.py
+++ sys_main/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Recruit(models.Model):
+    position = models.Field(max_length=255)
+    salary = models.Field(max_length=255)
+    company = models.Field(max_length=255)
+    experience = models.Field(max_length=255)
+    education = models.Field(max_length=255)
+    work_addr = models.Field(max_length=255)
+    company_size = models.Field(max_length=255)
+    job_info = models.TextField(max_length=255)
+    main_business = models.Field(max_length=255)
+    com_net = models.Field()
+    department = models.Field(max_length=255)
+    job_requirements = models.TextField()
+    status = models.Field(max_length=255)
+    create_time = models.Field(max_length=255)
+    class Meta:
+
+        db_table='t_recruit'
