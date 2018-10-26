480f3623f4a06dac6dd41fad790dde4dd2e9bb5d
zj <704972565@qq.com>
2018-10-26 08:56:59 +0800

    添加数据库配置

--- spider_team_project/settings.py
+++ spider_team_project/settings.py
@@ -77,9 +77,14 @@ WSGI_APPLICATION = 'spider_team_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'lib.mysql129',
+        'NAME': 'spiderteam',
+        'USER':'root',
+        'PASSWORD':'123456',
+        'HOST':'192.168.0.99',
+        'PORT':'3306'
+    },
+
 }
 
 
